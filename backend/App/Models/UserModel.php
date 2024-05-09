a0e416c446dac0094b25333df9fade0b38c80366
Zeberti19 <31765032+Zeberti19@users.noreply.github.com>
2024-05-09 15:35:48 +0400

    MySQL downgrade to 5.5.56

--- backend/App/Models/UserModel.php
+++ backend/App/Models/UserModel.php
@@ -30,36 +30,27 @@ class UserModel
     {
         $statement = DB::connect()->prepare(
             <<<SQL
-            with
-            transac(user_from_id, user_to_id, balance_changing, trdate) as
-            (
-                select ua_from.user_id as user_from_id,
-                       ua_to.user_id   as user_to_id,
-                       -t.amount       as balance_changing,
-                       trdate
-                from transactions t
-                     inner join user_accounts ua_from on t.account_from = ua_from.id
-                     inner join user_accounts ua_to on t.account_to = ua_to.id
-                union all
-                select ua_to.user_id   as user_from_id,
-                       ua_from.user_id as user_to_id,
-                       t.amount        as balance_changing,
-                       trdate
-                from transactions t
-                     inner join user_accounts ua_from on t.account_from = ua_from.id
-                     inner join user_accounts ua_to on t.account_to = ua_to.id
-            ),
-            data_main as
-            (
-            select date_format(trdate, '%Y-%m') as trdate,
-                   balance_changing
-            from transac
-            where user_from_id = :user_from_id and
-                  user_to_id != user_from_id
-            )
             select trdate,
                    sum(balance_changing) as balance
-            from data_main
+            from (
+                     select ua_from.user_id as user_from_id,
+                            ua_to.user_id as user_to_id,
+                            -t.amount as balance_changing,
+                            date_format(t.trdate, '%Y-%m') as trdate
+                     from transactions t
+                          inner join user_accounts ua_from on t.account_from = ua_from.id
+                          inner join user_accounts ua_to on t.account_to = ua_to.id
+                     union all
+                     select ua_to.user_id as user_from_id,
+                            ua_from.user_id as user_to_id,
+                            t.amount as balance_changing,
+                            date_format(t.trdate, '%Y-%m') as trdate
+                     from transactions t
+                          inner join user_accounts ua_from on t.account_from = ua_from.id
+                          inner join user_accounts ua_to on t.account_to = ua_to.id
+                 ) data_main
+            where user_from_id = :user_from_id and
+                  user_from_id != user_to_id
             group by trdate
             order by trdate
             SQL);
